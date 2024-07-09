9aeb3e72d14e6056fc45a08f88b43605294aca05
stoppedwumm <mobileboxnews@icloud.com>
2024-07-09 18:58:42 +0200

    Fix some stuff

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ const e = require("express")
 const app = e()
 const {kv} = require("@vercel/kv")
 
-app.get("/", (req, res) => {
-    res.send(kv.get("Test"))
+app.get("/", async (req, res) => {
+    res.send("WIP")
 })
 
-app.get("/v1/license", (req, res) => {
-    res.send("{}")
+app.get("/v1/license/:item/:userid", async (req, res) => {
+    const licenses = JSON.parse(await kv.get("license_"+item))
+    
+    if (licenses[req.query["userid"]] == 1) {
+        res.send("1")
+        return
+    } else {
+        res.send("0")
+        return
+    }
+    console.log("how is this even called?")
+    throw new Error("someting off")
 })
 
 app.listen(3000)
