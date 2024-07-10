a44d011bb16339b9a1eaafc347aac55c05eb3e8d
stoppedwumm <mobileboxnews@icloud.com>
2024-07-10 16:58:19 +0200

    Added some more code :)

--- index.js
+++ index.js
@@ -6,14 +6,47 @@ app.get("/", async (req, res) => {
     res.send("WIP")
 })
 
-app.get("/v1/license/:item/:userid", async (req, res) => {
-    const licenses = JSON.parse(await kv.get("license_"+req.query["item"]))
-    
-    if (licenses[req.query["userid"]] == 1) {
-        res.send("1")
-        return
-    } else {
+app.get("/v1/license", async (req, res) => {
+    let licenses = {}
+    try {
+        console.log("license_"+req.query["item"])
+        licenses = await kv.get("license_"+req.query["item"])
+        console.log(licenses)
+        if (licenses[req.query["userid"]] == 1) {
+            res.send("1")
+            return
+        } else {
+            res.send("0")
+            return
+        }
+    } catch (err) {
+        console.log(err)
+        console.log(licenses)
         res.send("0")
+        return
+    }
+    console.log("how is this even called?")
+    throw new Error("someting off")
+})
+
+app.post("/v1/addlicense", async (req, res) => {
+    let licenses = {}
+    try {
+        let header = req.header("auth")
+        header = header.replace("Bearer ", "")
+        if (header == process.env["APIToken"]) {
+            licenses = await kv.get("license_"+req.query["item"])
+            licenses[req.query["userid"]] = 1
+            await kv.set("license_"+req.query["item"], JSON.stringify(licenses))
+            res.sendStatus(201)
+            return
+        } else {
+            res.sendStatus(403)
+            return
+        }
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(403)
         return
     }
     console.log("how is this even called?")
